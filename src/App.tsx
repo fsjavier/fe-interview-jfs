af561bca30053d4bd893e9593b9d1546b7944a3c
fsjavier <javier.fersal@gmail.com>
2024-10-30 18:16:25 +0100

    Extract main layout to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
 import PostList from "./components/posts/PostList";
+import Layout from "./components/layout/Layout";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow container mx-auto mt-4">
-          <PostList />
-        </main>
-        <Footer />
-      </div>
+      <Layout>
+        <PostList />
+      </Layout>
     </QueryClientProvider>
   );
 }
